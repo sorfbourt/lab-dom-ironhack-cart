98e4c9f169ae0113d803f242451089695f268ac5
Krystina Witt <krystinawitt@gmail.com>
2023-01-18 17:04:58 +0100

    complete up till iteration 3

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
+  const price = product.querySelector(".price span").innerText
+  const quantity = product.querySelector(".quantity input").value
+  const subtotal = price*quantity
+
+  product.querySelector(".subtotal span").innerText = subtotal
+
+  return subtotal
 
   //... your code goes here
 }
@@ -9,16 +15,26 @@ function updateSubtotal(product) {
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+/*   const singleProduct = document.querySelector('.product');
+  updateSubtotal(singleProduct); */
   // end of test
 
   // ITERATION 2
-  //... your code goes here
+  const products = document.querySelectorAll('.product')
+ products.forEach(element => updateSubtotal(element))
 
   // ITERATION 3
-  //... your code goes here
+  const subtotal = document.querySelectorAll('.subtotal span')
+
+  let sum = 0;
+
+  subtotal.forEach((element)=>{
+    sum += parseInt(element.innerText)
+  }) 
+
+  document.querySelector('#total-value span').innerText = sum
 }
+
 
 // ITERATION 4
 
@@ -32,6 +48,9 @@ function removeProduct(event) {
 
 function createProduct() {
   //... your code goes here
+  console.log("check")
+  const newProductNameInput = document.querySelectorAll('.create-product td input')[0]
+  const newProductPriceInput = document.querySelectorAll('.create-product td input')[1]
 }
 
 window.addEventListener('load', () => {
@@ -40,3 +59,7 @@ window.addEventListener('load', () => {
 
   //... your code goes here
 });
+
+
+const addProduct = document.querySelector('#create')
+addProduct.addEventListener('click', createProduct)
